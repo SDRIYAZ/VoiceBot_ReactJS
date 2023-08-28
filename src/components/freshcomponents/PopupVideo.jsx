4787cfbff70f7bf2a43579e501c5bf201ce743b3
Sairam <sairamramavath01@gmail.com>
2023-08-28 17:08:49 +0530

    Updated Version 1

--- src/components/freshcomponents/PopupVideo.jsx
+++ src/components/freshcomponents/PopupVideo.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
-
 const PopupVideo = ({ videoUrl, onClose }) => {
     const [videoVisible, setVideoVisible] = useState(true);
-
     const handleVideoEnded = () => {
+        setVideoVisible(false);
+        onClose();
+    };
+    const handleCloseButtonClick = () => {
         setVideoVisible(false);
         onClose();
     };
@@ -15,13 +17,20 @@ const PopupVideo = ({ videoUrl, onClose }) => {
             videoElement.play();
         }
     }, [videoVisible]);
-
     return (
+        // <button className="close-button" onClick={handleCloseButtonClick} style={{width:"8rem", borderRadius:"10px",height : "2rem", marginTop: "25rem"}}>Close</button>
         <div className={`popup-video-container ${videoVisible ? 'visible' : 'hidden'}`}>
             <video id="popup-video" onEnded={handleVideoEnded}>
                 <source src={videoUrl} type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
+            <div className='popup_button'>
+
+                <button className="close-button" onClick={handleCloseButtonClick} style={{
+                    width: "8rem", borderRadius: "10px", height: "2rem", marginTop: "25rem", position: "absolute", bottom: "30rem",
+                    right: "10rem", background: "rgb(112 131 139)", color: "white"
+                }}>close</button>
+            </div>
         </div>
     );
 };
